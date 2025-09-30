03640e3fcd09e40d0030ede50c96ae05fb74b912
walnutmaru <154221078+22400225@users.noreply.github.com>
2025-10-01 01:07:57 +0900

    Lab1

---
+++ react-hello-project/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="main-title">My First React Lab Test!</h1> 
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <button className="test-button" onClick={() => alert('버튼이 클릭되었습니다!')}>
+          Click Me!
+        </button>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
